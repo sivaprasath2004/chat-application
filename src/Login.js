12e2f175c1baa5b768a9ff8e6dc8927e1b94f5ba
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-02-26 07:20:43 +0530

    Login Page

--- src/Login.js
+++ src/Login.js
@@ -4,17 +4,17 @@ const Login = () => {
     const [checker,setChecker]=useState({})
   return (
    <div id='home_page'  style={{display:'flex',justifyContent:'center',alignItems:'center',height:'100vh'}}>
-    <div style={{display:'flex',flexWrap:'wrap-reverse',justifyContent:'center',alignItems:'center'}}>
-    <form style={{flex:'1 0 8rem',display:'flex',flexDirection:'column'}}>
+    <form style={{width:'100%',display:'flex',justifyContent:'center',alignItems:'center'}} >
+    <div style={{display:'flex',flexDirection:'column',position:'relative',justifyContent:'center',alignItems:'center',maxWidth:'80%',minWidth:'40%',height:300,padding:20,backgroundColor:'cyan'}}>
+      <div id='background'> </div>
     <h1>Login Room</h1>
         <input type='text' onChange={(e)=>setChecker(pre=>({...pre,name:e.target.value}))}/>
         <input type='text' onChange={(e)=>setChecker(pre=>({...pre,password:e.target.value}))}/>  
         <Link  onClick={(e)=>(!checker.name || !checker.password)?e.preventDefault():null}
         to={`/chat?name=${checker.name}&room=${checker.password}`}>
         <input type='submit' value={`submit`} /></Link>
-    </form>
-    <img  src='https://th.bing.com/th/id/OIG4.prqOvMVwWY0myCDIW1iF?w=1024&h=1024&rs=1&pid=ImgDetMain' style={{flex:'1 0 8rem',objectFit:'contain',height:300,width:300}} alt='image'/>
-    </div>
+    
+    </div></form>
    </div>
   )
 }
