9dcf4763d52c3055f5b1ff9e32f0ed464b046d2e
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-02-26 22:01:35 +0530

    login

--- src/Login.js
+++ src/Login.js
@@ -5,15 +5,17 @@ const Login = () => {
   return (
    <div id='home_page'  style={{display:'flex',justifyContent:'center',alignItems:'center',height:'100vh'}}>
     <form style={{width:'100%',display:'flex',justifyContent:'center',alignItems:'center'}} >
-    <div style={{display:'flex',flexDirection:'column',position:'relative',justifyContent:'center',alignItems:'center',maxWidth:'80%',minWidth:'40%',height:300,padding:20,backgroundColor:'cyan'}}>
-      <div id='background'> </div>
-    <h1>Login Room</h1>
-        <input type='text' onChange={(e)=>setChecker(pre=>({...pre,name:e.target.value}))}/>
-        <input type='text' onChange={(e)=>setChecker(pre=>({...pre,password:e.target.value}))}/>  
+    <div id="background_circle">
+    <div className='circle'></div>
+   <div className='form'> <h1>Login Room</h1>
+        <input placeholder='User name' className='name' type='text' onChange={(e)=>setChecker(pre=>({...pre,name:e.target.value}))}/>
+        <input placeholder='Room Id' className='room' type='text' onChange={(e)=>setChecker(pre=>({...pre,password:e.target.value}))}/>  
         <Link  onClick={(e)=>(!checker.name || !checker.password)?e.preventDefault():null}
         to={`/chat?name=${checker.name}&room=${checker.password}`}>
         <input type='submit' value={`submit`} /></Link>
-    
+        <div id='star'></div>
+        <div id='stars'></div>
+        </div>
     </div></form>
    </div>
   )
